8e5ddbd87429b6b9bf4f7349e20a3403fe12d05b
Tu Tong <ttwoosta@gmail.com>
2019-08-31 14:03:46 -0400

    Intialial stage

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int count;
+	
+	cout << "Hello" << endl;
+	
+	cin >> count;
+	
+	cout << "Count: " << count << endl;
+	
+	return 0;
+}
