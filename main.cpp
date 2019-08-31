bc1fa26d5a6e4e325f821aed0829a840fdbf9c66
Tu Tong <ttwoosta@gmail.com>
2019-08-31 14:30:36 -0400

    Printed the 1st laptop

--- main.cpp
+++ main.cpp
@@ -3,15 +3,61 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 
+// Date class
+class Date {
+	public:
+		void output();
+		int day;
+		int month;
+		int year;
+};
+
+// Laptop class
+class Laptop {
+	public:
+		void output();
+		string brand;
+		int ram;
+		Date productionDate;
+		int hardDriveCapacity;
+		float screenSize;
+		float price;
+};
+
+
 int main(int argc, char** argv) {
 	
-	int count;
+	Laptop laptops[3];
+	Date dates[3];
 	
-	cout << "Hello" << endl;
+	dates[0].day = 22;
+	dates[0].month = 11;
+	dates[0].year = 2019;
 	
-	cin >> count;
-	
-	cout << "Count: " << count << endl;
+	laptops[0].brand = "Acer Chromebook";
+	laptops[0].productionDate = dates[0];
+	laptops[0].ram = 16;
+	laptops[0].hardDriveCapacity = 512;
+	laptops[0].screenSize = 11.5;
+	laptops[0].price = 217.17;
+
+	laptops[0].output();
 	
 	return 0;
+}
+
+// Use iostream
+void Date::output() {
+	std::cout << month << "/" << day << "/" << year << std::endl;
+}
+
+void Laptop::output() {
+	std::cout << brand << ", " << screenSize << "\", " 
+	<< ram << "GB RAM, " << hardDriveCapacity 
+	<< "GB HHD, $" << price
+	<< ", Release Date: ";
+	
+	productionDate.output();
+	
+	cout << std::endl;
 }
